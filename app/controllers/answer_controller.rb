78ac93f52685ea3e66c84bf525adb02ea8de6dde
Rahul <rahul@rahulswaminathan.com>
2011-09-17 14:37:52 -0700

    actually all working now

--- app/controllers/answer_controller.rb
+++ app/controllers/answer_controller.rb
@@ -1,2 +1,13 @@
 class AnswerController < ApplicationController
+    def create
+        @presentation = Presentation.find(params[:presentation_id])
+        @question = Question.find(params[:question_id])
+        @answer= @question.answers.build(params[:answer])
+        if @answer.save
+            flash[:success] = "Saved your answer. Answer another question. "
+        else
+            flash[:error] = "Something went wrong when saving your answer. Try again."
+        end
+        redirect_to presentation_path
+    end
 end

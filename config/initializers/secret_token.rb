d3ea3a1c1c7bd0cab3921eeec44bf06f91e58e7e
Rahul <rahul@rahulswaminathan.com>
2011-09-17 02:39:15 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pollinate::Application.config.secret_token = '1610c82f164ce29369dfcd316bb5d0191235f8f8465feb1707034c5c9e2cb6fc2a3ccf172db6e6b21339a11acad1bd69c734e483453162354a6a8224dbc4780c'

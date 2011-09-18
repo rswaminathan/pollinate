886f909a67f00cf0d102d74d82b193db59cbb4d3
Rahul <rahul@rahulswaminathan.com>
2011-09-17 19:39:28 -0700

    test

--- config/routes.rb
+++ config/routes.rb
@@ -14,12 +14,6 @@ Pollinate::Application.routes.draw do
 
 
   # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
 
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
